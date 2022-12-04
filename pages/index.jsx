e4ca04f56839c50e184edada5c80b80d32890a86
DezyNation <dezynationindia@gmail.com>
2022-12-04 21:40:31 +0530

    masonry pending

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import React from 'react'
 import Navbar from '../hocs/Nav'
-import {Flex, Image, Text, Button, Box} from '@chakra-ui/react'
+import { Flex, Image, Text, Button, Box } from '@chakra-ui/react'
 
 const Index = () => {
   return (
@@ -11,59 +11,100 @@ const Index = () => {
       </Head>
       <Navbar />
       <Flex
-      minH={['60vh', '80vh', '100vh']}
-      p={[4,16]} bg={"url('cubes.svg') center/cover no-repeat"}
-      bgPos={['65% 50%', 'center', 'center']}
-      direction={'column'}
-      alignItems={'center'}
-      justifyContent={'center'}
+        minH={['60vh', '80vh', '100vh']}
+        p={[4, 16]} bg={"url('cubes.svg') center/cover no-repeat"}
+        bgPos={['65% 50%', 'center', 'center']}
+        direction={'column'}
+        alignItems={'center'}
+        justifyContent={'center'}
       >
-        <Text fontSize={[36, 48, 60]} 
-        textTransform={'capitalize'}
-        fontWeight={600}
-        textAlign={'center'}
+        <Text fontSize={[36, 48, 60]}
+          textTransform={'capitalize'}
+          fontWeight={600}
+          textAlign={'center'}
         >
-          find <span style={{color: 'blue'}}>your</span> dream freelance team
+          find <span style={{ color: 'blue' }}>your</span> dream freelance team
         </Text>
         <Text
-        my={4}
-        textAlign={'center'}
-        fontSize={[20,24,28]}
-        color={'#555'}
+          my={4}
+          textAlign={'center'}
+          fontSize={[20, 24, 28]}
+          color={'#555'}
         >
-          Hire trustworthy freelancers from all over India for your business project
+          Hire trustworthy freelancers from all over India for your next project
         </Text>
-        <Button my={4} rounded={'full'} size={'lg'} 
-        colorScheme={'messenger'} bg={'blue'}
-        shadow={'xl'}
+        <Button my={4} rounded={'full'} size={'lg'}
+          colorScheme={'messenger'} bg={'blue'}
+          shadow={'xl'}
         >
           Search Freelancers
         </Button>
       </Flex>
 
-      <Flex 
-      h={['auto', '70vh', '80vh']}
-      alignItems={'center'}
-      direction={['column', 'row']}
-      p={[4,16]}
+      <Flex
+        h={['auto', '70vh', '80vh']}
+        alignItems={'center'}
+        direction={['column', 'row']}
+        p={[4, 16]}
       >
         <Box
-        maxW={['full', '50%', '40%']}
+          maxW={['full', '50%', '40%']}
         >
           <Text
-          fontSize={[24,30,36]}
-          fontWeight={600}
+            fontSize={[24, 30, 36]}
+            fontWeight={600}
           >
-            One platform for<br/>
+            One platform for<br />
             all your business needs
           </Text>
           <Text my={4}>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo ex ad fugiat, architecto quae adipisci commodi tempore doloremque culpa consequatur.
           </Text>
-        <Button my={4} rounded={'full'} size={'lg'} colorScheme={'teal'} bg={'#222'} _hover={{bg: '#333'}}>
-          Search Freelancers
-        </Button>
+          <Button my={4} rounded={'full'} size={'lg'} colorScheme={'teal'} bg={'#222'} _hover={{ bg: '#333' }}>
+            Post Your Project
+          </Button>
         </Box>
+      </Flex>
+
+      <Flex
+        h={['auto', '70vh', '80vh']}
+        alignItems={'center'}
+        direction={['column', 'row-reverse']}
+        p={[4, 16]}
+      >
+        <Box
+          maxW={['full', '50%', '40%']}
+        >
+          <Text
+            fontSize={[24, 30, 36]}
+            fontWeight={600}
+          >
+            Pay only when<br />
+            you get the work done
+          </Text>
+          <Text my={4}>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo ex ad fugiat, architecto quae adipisci commodi tempore doloremque culpa consequatur.
+          </Text>
+          <Button my={4} rounded={'full'} size={'lg'} colorScheme={'teal'} bg={'#222'} _hover={{ bg: '#333' }}>
+            Post Your Project
+          </Button>
+        </Box>
+      </Flex>
+
+      <Flex
+        direction={'column'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        p={[4,16]}
+      >
+        <Text
+          fontSize={[24, 30, 36]}
+          fontWeight={600}
+          mb={8}
+        >
+          See what we have creates so far...
+        </Text>
+
       </Flex>
     </>
   )
